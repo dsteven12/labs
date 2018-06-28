f231197561be2040ee9512aa65d31c58a0c61965
Darryl Lardizabal <dsteven12@gmail.com>
2018-06-28 09:47:08 -0700

    Added Composer, and setup Whoops and Kint

--- app/public/wp-content/plugins/ktc-starter/plugin.php
+++ app/public/wp-content/plugins/ktc-starter/plugin.php
@@ -10,7 +10,7 @@
  * @wordpress-plugin
  * Plugin Name: Starter WP Plugin
  * Plugin URI:  https://example.com/plugin-name
- * Description: Know the Code Starter Sandbox plugin. Use this plugin for all of the Know the Code demo labs and DOCX.
+ * Description: Know the Code Starter Sandbox plugin. Use this plugin for all of the Know the Code demo labs and DocX.
  * Version:     1.0.0
  * Author:      Darryl Lardizabal
  * Author URI:  https://example.com
@@ -20,3 +20,21 @@
  */
 
 namespace LearnThePlug;
+
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require_once( __DIR__ . '/assets/vendor/autoload.php' );
+
+$whoops = new Run();
+$error_page = new PrettyPageHandler();
+$error_page->setEditor( 'sublime' );
+$whoops->pushHandler( $error_page );
+$whoops->register();
+
+func_num_args();
+
+add_action( 'loop_start', __NAMESPACE__ . '\demo' );
+function demo() {
+	d( get_the_ID() );
+}
